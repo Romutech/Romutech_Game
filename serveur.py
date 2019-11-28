c7f5b8de4e87a4318d58c66b66812f38322b7b20
Robin Muller <robin.muller.romutech@gmail.com>
2019-11-28 05:31:07 +0100

    management multi-client step 3

--- serveur.py
+++ serveur.py
@@ -59,40 +59,129 @@ while True:
 		serveur_lance = True
 		clients_connectes = []
 		while serveur_lance:
-		    # On va vérifier que de nouveaux clients ne demandent pas à se connecter
-		    # Pour cela, on écoute la connexion_principale en lecture
-		    # On attend maximum 50ms
-		    connexions_demandees, wlist, xlist = select.select([connexion_principale],
-		        [], [], 0.05)
-		    
-		    for connexion in connexions_demandees:
-		        connexion_avec_client, infos_connexion = connexion.accept()
-		        # On ajoute le socket connecté à la liste des clients
-		        clients_connectes.append(connexion_avec_client)
-		    
-		    # Maintenant, on écoute la liste des clients connectés
-		    # Les clients renvoyés par select sont ceux devant être lus (recv)
-		    # On attend là encore 50ms maximum
-		    # On enferme l'appel à select.select dans un bloc try
-		    # En effet, si la liste de clients connectés est vide, une exception
-		    # Peut être levée
-		    clients_a_lire = []
-		    try:
-		        clients_a_lire, wlist, xlist = select.select(clients_connectes,
-		                [], [], 0.05)
-		    except select.error:
-		        pass
-		    else:
-		        # On parcourt la liste des clients à lire
-		        for client in clients_a_lire:
-		            # Client est de type socket
-		            msg_recu = client.recv(1024)
-		            # Peut planter si le message contient des caractères spéciaux
-		            msg_recu = msg_recu.decode()
-		            print("Reçu {}".format(msg_recu))
-		            client.send(text.encode())
-		            if msg_recu == "fin":
-		                serveur_lance = False
+			# On va vérifier que de nouveaux clients ne demandent pas à se connecter
+			# Pour cela, on écoute la connexion_principale en lecture
+			# On attend maximum 50ms
+			connexions_demandees, wlist, xlist = select.select([connexion_principale], [], [], 0.05)
+			
+			for connexion in connexions_demandees:
+				connexion_avec_client, infos_connexion = connexion.accept()
+				# On ajoute le socket connecté à la liste des clients
+				clients_connectes.append(connexion_avec_client)
+			
+			# Maintenant, on écoute la liste des clients connectés
+			# Les clients renvoyés par select sont ceux devant être lus (recv)
+			# On attend là encore 50ms maximum
+			# On enferme l'appel à select.select dans un bloc try
+			# En effet, si la liste de clients connectés est vide, une exception
+			# Peut être levée
+			clients_a_lire = []
+			try:
+				clients_a_lire, wlist, xlist = select.select(clients_connectes, [], [], 0.05)
+			except select.error:
+				pass
+			else:
+
+				# On parcourt la liste des clients à lire
+				for client in clients_a_lire:
+					client.send(text.encode())
+					text =""
+
+					# Client est de type socket
+					msg_recu = client.recv(1024)
+					# Peut planter si le message contient des caractères spéciaux
+					msg_recu = msg_recu.decode()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				
+					
+					
+					i = 0
+
+					
+
+					order = msg_recu
+
+					if order.upper() == 'Q':
+						loop = False
+						break
+
+					if robot.the_direction_is_valid(order) == False or robot.number_of_move_box_is_valid(order) == False:
+						continue
+					
+					if len(order[1:]) == 0:
+						number_of_boxes = 1
+					else:
+						number_of_boxes = int(order[1:])
+
+					i = 0
+
+					letter = str(order[0])
+
+					old_location = robot.get_position()
+
+					while i < number_of_boxes:
+						position = robot.displacement(letter, labyrinth)
+
+						i += 1
+
+						result = labyrinth.positioning_is_validated(position)
+
+						if result == False:
+							robot.set_position(old_location)
+							text = "[status]" + "Impossible d'aller là !"
+							break
+
+						if result == True:
+							robot.set_position(position)
+							labyrinth.clear_the_robot_in_maze(labyrinth.grille)
+							data = labyrinth.show(labyrinth.grille, chosen_card.height, chosen_card.width, position)
+
+						text = "[labyrinth]" + data
+
+					if labyrinth.is_win(position):
+						win = True
+						client.send("[win] Bravo ! \nVous avez gagné !".encode())
+						# connection_with_client.close()
+						# connection.close()
+						break
+					if win == False:
+						client.send(text.encode())
+
+
+
+
+
+
+
+
+
+
+
+
+					print("Reçu {}".format(msg_recu))
+					
+					if msg_recu == "fin":
+					    serveur_lance = False
 
 
 		# connection_with_client, infos_connexion = connection.accept()
@@ -100,62 +189,7 @@ while True:
 
 		
 
-		while win == False:
-			message_received = connection_with_client.recv(1024)
-			message_received = message_received.decode()
-			
-			i = 0
-
-			
-
-			order = message_received
-
-			if order.upper() == 'Q':
-				loop = False
-				break
-
-			if robot.the_direction_is_valid(order) == False or robot.number_of_move_box_is_valid(order) == False:
-				continue
-			
-			if len(order[1:]) == 0:
-				number_of_boxes = 1
-			else:
-				number_of_boxes = int(order[1:])
-
-			i = 0
-
-			letter = str(order[0])
-
-			old_location = robot.get_position()
-
-			while i < number_of_boxes:
-				position = robot.displacement(letter, labyrinth)
-
-				i += 1
-
-				result = labyrinth.positioning_is_validated(position)
-
-				if result == False:
-					robot.set_position(old_location)
-					text = "[status]" + "Impossible d'aller là !"
-					break
-
-				if result == True:
-					robot.set_position(position)
-					labyrinth.clear_the_robot_in_maze(labyrinth.grille)
-					data = labyrinth.show(labyrinth.grille, chosen_card.height, chosen_card.width, position)
-
-				text = "[labyrinth]" + data
-
-			if labyrinth.is_win(position):
-				win = True
-				connection_with_client.send("[win] Bravo ! \nVous avez gagné !".encode())
-				# connection_with_client.close()
-				# connection.close()
-				break
-			if win == False:
-				connection_with_client.send(text.encode())
-
+		
 		print("Fermeture de la connexion")
 		connection_with_client.close()
 		connection.close()
