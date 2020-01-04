53785e4973bb1bc39020a105df6dd214266313eb
Robin Muller <robin.muller.romutech@gmail.com>
2020-01-04 20:53:10 +0100

    wip

--- serveur.py
+++ serveur.py
@@ -1,18 +1,89 @@
+# -*-coding:Utf-8 -*
+
+"""Ce fichier contient le code principal du jeu.
+
+Exécutez-le avec Python pour lancer le jeu.
+
+"""
+
+import os
 import socket
 import select
 import time
 
+from carte import Carte
+from labyrinthe import Labyrinthe
+from class_robot.robot import Robot
+
 hote = ''
 port = 12800
+
+# On charge les cartes existantes
+cartes = []
+for nom_fichier in os.listdir("cartes"):
+    if nom_fichier.endswith(".txt"):
+        chemin = os.path.join("cartes", nom_fichier)
+        nom_carte = nom_fichier[:-3].lower()
+        with open(chemin, "r") as fichier:
+            contenu = fichier.read()
+            # Création d'une carte, à compléter
+            cartes.append(Carte(nom_carte, contenu))
+
+# On affiche les cartes existantes
+print("Labyrinthes existants :")
+for i, carte in enumerate(cartes):
+    print("  {} - {}".format(i + 1, carte.nom))
+
+number_of_cards = i + 1
+
+#continuation of the program 
+
+win = False
+loop = True
+first = True
+response = 'OUI'
+chosen_card = {}
+
+while True:
+	try:
+		choose = int(input("\nEntrez un numéro de labyrinthe pour commencer à jouer : "))
+
+		if 0 == choose :
+			raise IndexError
+			
+		chosen_card = cartes[choose-1]
+		labyrinth = Labyrinthe(chosen_card.labyrinthe, 'X', 'O', '.', 'U', carte.nom, chosen_card.height, chosen_card.width)
+		
+		while True:
+			starting_position_of_the_robot = labyrinth.determine_starting_position_from_map(labyrinth.grille)
+			robot = Robot(starting_position_of_the_robot)
+			if labyrinth.positioning_is_validated((robot.ordinate, robot.abscissa)) == True:
+				break
+		
+	except ValueError as e:
+		print("Veuillez saisir un nombre")
+	except IndexError as e:
+		print("Veuillez saisir une carte qui existante")
+	else:
+		break
+
+# ------------------------------------ PARTIE SERVEUR ------------------------------------------------------------------
 
 connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_principale.bind((hote, port))
 connexion_principale.listen(5)
 print("Le serveur écoute à présent sur le port {}".format(port))
 
+
 serveur_lance = True
 clients_connectes = []
-while serveur_lance:
+
+# ------------------------------------ FIN PARTIE SERVEUR --------------------------------------------------------------
+
+
+while win == False and loop and serveur_lance:
+
+# ------------------------------------ PARTIE SERVEUR ------------------------------------------------------------------
 	connexions_demandees, wlist, xlist = select.select([connexion_principale], [], [], 0.05)
 	
 	for connexion in connexions_demandees:
@@ -20,6 +91,16 @@ while serveur_lance:
 		clients_connectes.append(connexion_avec_client)
 	
 	clients_a_lire = []
+
+	if first:
+		first = False
+		message = labyrinth.show(labyrinth.grille, chosen_card.height, chosen_card.width, robot.get_position()) 
+
+		for c in clients_connectes:
+			if len(msg_recu) > 0:
+				c.send(message.encode())
+
+
 	try:
 		clients_a_lire, wlist, xlist = select.select(clients_connectes, [], [], 0.05)
 	except select.error:
@@ -32,17 +113,63 @@ while serveur_lance:
 			msg_recu = msg_recu.decode()
 			print("Reçu {}".format(msg_recu))
 
-			for c in clients_connectes:
-				if len(msg_recu) > 0:
-					c.send(msg_recu.encode())
+			order = msg_recu
+			i = 0
+			 
+			if order.upper() == 'Q':
+				print('Fin du jeu ! Au revoir !')
+				loop = False
+				break
+
+			if robot.the_direction_is_valid(order) == False or robot.number_of_move_box_is_valid(order) == False:
+				continue
+
+			if len(order[1:]) == 0:
+				number_of_boxes = 1
+			else:
+				number_of_boxes = int(order[1:])
+
+			while i < number_of_boxes:
+				position = robot.displacement(order)
+
+				if labyrinth.positioning_is_validated(position) == False:
+					client.send("vous ne pouvez pas aller à cet endroit car un obstacle vous en empeche ! ".encode())
+					break
+
+				robot.set_position(position)
+				labyrinth.clear_the_robot_in_maze(labyrinth.grille)
+				message = labyrinth.show(labyrinth.grille, chosen_card.height, chosen_card.width, robot.get_position())
+
+				if labyrinth.is_win(position):
+					win = True
+					message = "\n\n  *  *  *\n   \ | /\n *-OOOO-*  *************************************\n  OOO      * Félicitations ! Vous avez gagné ! *\n OO        *************************************\nO\n"
+
+				for c in clients_connectes:
+					if len(msg_recu) > 0:
+						c.send(message.encode())
+
+				i += 1
+			
 
 		if len(msg_recu) > 0:
 			if msg_recu == "fin":
 				serveur_lance = False
+# ------------------------------------ FIN PARTIE SERVEUR --------------------------------------------------------------
+
+		
+
+# toto deplacer dans client => str(input("Saisissez une lettre pour déplacer le robot 'n' 's' 'e' 'o' ou saisissez 'q' pour quitter le jeu: "))
+
+# ------------------------------------ PARTIE SERVEUR ------------------------------------------------------------------
 
 print("Fermeture des connexions")
 
 for client in clients_connectes:
-    client.close()
+	client.close()
 
 connexion_principale.close()
+
+# ------------------------------------ FIN PARTIE SERVEUR --------------------------------------------------------------
+
+
+	
