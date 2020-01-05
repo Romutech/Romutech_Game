4ea0c1f49962d2f45e9d5feaf1bc450f3ce63b2b
Robin Muller <robin.muller.romutech@gmail.com>
2020-01-05 15:18:21 +0100

    management multi bot step 2

--- carte.py
+++ carte.py
@@ -9,7 +9,7 @@ class Carte:
     def __init__(self, nom, chaine):
 
         """Initializes the representation of a map.
-        Receives as parameter the name of a map in the form of a string of characters 
+        Receives as parameter the name of a map in the form of a string of characters
         and a string representing the labyrinth."""
 
         self.nom = nom
@@ -21,7 +21,7 @@ class Carte:
 
 
     def creer_labyrinthe_depuis_chaine(self, chaine):
-        
+
         """This function makes a maze from a string of characters and takes a string of characters.`
         She is returning a dictionary"""
 
@@ -42,7 +42,7 @@ class Carte:
         return grille
 
     def convert_labyrinth_from_dictionary_to_string(self, dictionnary, grid_height, grid_width):
-        
+
         """converts dictionary-type labyrinth into a character string"""
 
         i = 0 # abscissa
@@ -50,17 +50,16 @@ class Carte:
 
         string_map = ""
 
-        while j < grid_width: 
+        while j < grid_width:
 
             while i < grid_height:
                 string_map += dictionnary[j, i]
                 i += 1
 
             string_map += "\n"
-      
+
             i = 0
             j += 1
 
         self.labyrinthe = self.creer_labyrinthe_depuis_chaine(string_map)
 
-    
--- labyrinthe.py
+++ labyrinthe.py
@@ -80,7 +80,7 @@ class Labyrinthe:
 
         # if the robot was not found on the map, this position is determined randomly
         print ("définition aléatoire de la position du robot")
-        return (randrange(0, self.width), randrange(0, self.height))
+        return (randrange(0, self.width), randrange(0, self.height -1))
 
 
     def clear_the_robot_in_maze(self, labyrinth):
